54630f7d07d73ea72e333cbb4848ac7968db0d2e
ispasdani <ispasdani14@gmail.com>
2026-06-16 23:00:02 +0200

    floating nav done

--- packages/ui-web/src/components/marketing-general/navbars/navbar.tsx
+++ packages/ui-web/src/components/marketing-general/navbars/navbar.tsx
@@ -2,12 +2,14 @@ import { Container } from "@workspace/ui-shared/components/marketing-general/con
 import { DesktopNav } from "./desktop-nav"
 import { MobileNav } from "./mobile-nav"
 import { items } from "./data"
+import { FloatingNav } from "./floating-nav"
 
 export const Navbar = () => {
   return (
     <Container as="nav">
       <DesktopNav items={items} />
       <MobileNav items={items} />
+      <FloatingNav items={items} />
     </Container>
   )
 }
